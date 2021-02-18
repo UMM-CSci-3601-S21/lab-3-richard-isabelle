5b495c94a8c733260e7309afced2889154f8232f
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-02-17 17:54:15 -0900

    Create the todo page
    Created the todo-list and todos-profile components, and made modifications to the app component typescript and HTML to access the page.

---
+++ client/src/app/todos/todos-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-list',
+  templateUrl: './todos-list.component.html',
+  styleUrls: ['./todos-list.component.scss'],
+  providers: []
+})
+export class TodosListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
