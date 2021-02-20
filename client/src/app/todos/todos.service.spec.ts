b0b3d4d3fca10ff75eaec1bfcc91f42071b23944
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-02-20 14:22:22 -0900

    Fix todo.service.spec test
    Modified the code and the tests pass.

--- client/src/app/todos/todos.service.spec.ts
+++ client/src/app/todos/todos.service.spec.ts
@@ -1,17 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Todos } from './todos';
 import { TodosService } from './todos.service';
 
 describe('TodosService', () => {
-  let service: TodosService;
+
+  let todosService: TodosService;
+  // Three todos
+  const testTodos: Todos[] = [
+    {
+      _id: '58895985a22c04e761776d54',
+      owner: 'Blanche',
+      status: 'incomplete',
+      body: 'In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.',
+      category: 'software design'
+    },
+    {
+      _id: '58895985c1849992336c219b',
+      owner: 'Fry',
+      status: 'incomplete',
+      body: 'Ipsum esse est ullamco magna tempor anim laborum non officia deserunt veniam commodo. Aute minim incididunt ex commodo.',
+      category: 'video games'
+    },
+    {
+      _id: '58895985ae3b752b124e7663',
+      owner: 'Fry',
+      status: 'complete',
+      body: 'Ullamco irure laborum magna dolor non. Anim occaecat adipisicing cillum eu magna in.',
+      category: 'homework'
+    }
+  ];
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodosService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    todosService = new TodosService(httpClient);
   });
-/*
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(todosService).toBeTruthy();
   });
-  */
+
 });
