1dfd5ae38a5ebde302d3d30184c9dce7ac906deb
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-02-19 19:01:30 -0900

    Edit in todo-list HTML and create filtering methods
    Edited the HTML to show the filtering options and test boxes. Also created the method in in todo-list.components to enable filtering.

--- client/src/app/todos/todos-list.component.ts
+++ client/src/app/todos/todos-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Todos , TodosStatus } from './todos';
 import { TodosService } from './todos.service';
 
@@ -18,31 +19,36 @@ export class TodosListComponent implements OnInit {
   public todosBody: string;
   public todosCategory: string;
 
-  constructor(private todosService: TodosService) { }
-/*
+  constructor(private todosService: TodosService, private snackBar: MatSnackBar) { }
+
   getTodosFromServer() {
     this.todosService.getTodos({
       owner: this.todosOwner,
       status: this.todosStatus,
-      body: this.todosBody,
-      category: this.todosCategory,
+      body: this.todosBody
     }).subscribe(returnedTodos => {
       this.serverFilteredTodos = returnedTodos;
       this.updateFilter();
+    }, err => {
+      this.snackBar.open(
+        'Problem contacting the server - please try again',
+        'OK',
+        {duration: 3000});
     });
   }
 
-  /**
+  /*
    * This method filters through the client side
-  /
+  */
   public updateFilter(){
     this.filteredTodos = this.todosService.filterTodos(
-      this.serverFilteredTodos, { owner: this.todosOwner,
-      category: this.todosCategory });
+      this.serverFilteredTodos, { category: this.todosCategory });
   }
-*/
+
+  /**
+   * Starts an asynchronous operation to update the todos list
+   */
   ngOnInit(): void {
+    this.getTodosFromServer();
   }
-
-
 }
--- client/src/app/todos/todos.service.ts
+++ client/src/app/todos/todos.service.ts
@@ -11,4 +11,39 @@ export class TodosService {
   readonly todosUrl: string = environment.apiUrl + 'todos';
 
   constructor(private httpClient: HttpClient) { }
+
+  getTodos(filters?: { status?: TodosStatus; owner?: string; body?: string}): Observable<Todos[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if(filters) {
+      if(filters.owner) {
+        httpParams = httpParams.set('owner', filters.owner);
+      }
+      if(filters.body) {
+        httpParams = httpParams.set('body', filters.body);
+      }
+      if(filters.status) {
+        httpParams = httpParams.set('status', filters.status);
+      }
+    }
+    return this.httpClient.get<Todos[]>(this.todosUrl, {
+      params: httpParams,
+    });
+  }
+
+  getTodoById(id: string): Observable<Todos> {
+    return this.httpClient.get<Todos>(this.todosUrl + '/' + id);
+  }
+
+  filterTodos(todos: Todos[], filters: { category?: string }): Todos[] {
+
+    let filteredTodos = todos;
+
+    if(filters.category) {
+      filters.category = filters.category.toLowerCase();
+
+      filteredTodos = filteredTodos.filter(todo => todo.category.toLowerCase().indexOf(filters.category) !== -1);
+    }
+
+    return filteredTodos;
+  }
 }
