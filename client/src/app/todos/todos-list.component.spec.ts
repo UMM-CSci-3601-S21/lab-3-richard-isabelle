f9c45bff0d6b05ec74bcf9f257121d6ac501c316
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-02-20 14:14:59 -0900

    Fix todo-list.component.spec test
    Created the todos.service.mock.ts and fixed the failing test in the todos-list.component.spec file

--- client/src/app/todos/todos-list.component.spec.ts
+++ client/src/app/todos/todos-list.component.spec.ts
@@ -1,6 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockTodosService } from '../../testing/todos.service.mock';
+import { Todos } from './todos';
 import { TodosListComponent } from './todos-list.component';
+import { TodosService } from './todos.service';
+
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
 
 describe('TodosListComponent', () => {
   let component: TodosListComponent;
@@ -8,7 +44,9 @@ describe('TodosListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodosListComponent ]
+      imports: [COMMON_IMPORTS],
+      declarations: [ TodosListComponent ],
+      providers: [{ provide: TodosService, useValue: new MockTodosService() }]
     })
     .compileComponents();
   });
@@ -18,9 +56,8 @@ describe('TodosListComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-/*
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  */
 });
